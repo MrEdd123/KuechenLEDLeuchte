5dfc078f70b8497ebf0a305a93a6d887e480c706
Andre Reichl <andre.reichl@gmx.de>
2018-01-13 12:30:13 +0100

    Startwert für LED im EEPROM speichern mit beiden Tasten gleichzeitig drücken.

--- KüchenLED_Regelung.ino
+++ KüchenLED_Regelung.ino
@@ -3,6 +3,7 @@
 // variablen
 
 
+#include <EEPROM.h>
 uint16_t PWM = 800;
 uint8_t Hoch = 0;
 uint8_t Runter = 0;
@@ -10,26 +11,31 @@ uint8_t Runter = 0;
 
 void setup()
 {
-	
+	Serial.begin(115200);
+	EEPROM.begin(64);
 	pinMode(D6, OUTPUT);
-	analogWrite(D6, PWM);				// LED Startwert 
+	 
 	pinMode(D3, INPUT_PULLUP);
 	pinMode(D2, INPUT_PULLUP);
 	pinMode(D1, OUTPUT);				//Blink LED
+	digitalWrite(D1, HIGH);
 	
+	EEPROM.get(1, PWM);
+	EEPROM.end();
+	analogWrite(D6, PWM);				// LED Startwert
 }
 
 void loop()
 {
-
+	Serial.println(PWM);
 	 //Tasterrauf abfrage
 	Hoch = digitalRead(D3);
-	if (Hoch == LOW) {
+	if (Hoch == LOW & Runter == HIGH) {
 
 		if (PWM < 1023, PWM++);
 		if (PWM >= 1024) { PWM = 1023; blink(); }
-		/*Serial.print("Rauf:");
-		Serial.println(PWM);*/
+		Serial.print("Rauf:");
+		Serial.println(PWM);
 		analogWrite(D6, PWM);
 		
 		delay(10);
@@ -39,15 +45,23 @@ void loop()
 
 	//Tasterunter abfrage
 	Runter = digitalRead(D2);
-	if (Runter == LOW) {
+	if (Runter == LOW & Hoch == HIGH) {
 
 		if (PWM >= 1023, PWM--);
-		if (PWM <= 21) { PWM = 20; blink() ; }
-		/*Serial.print("Runter:");
-		Serial.println(PWM);*/
+		if (PWM <= 11) { PWM = 10; blink() ; }
+		Serial.print("Runter:");
+		Serial.println(PWM);
 		analogWrite(D6, PWM);
 
 		delay(10);
+	}
+
+	if (Hoch == LOW & Runter == LOW) {
+		
+		blink();
+		EEPROM.put(1, PWM);
+		EEPROM.commit();
+		EEPROM.end();
 	}
 }
 
@@ -55,9 +69,9 @@ void blink() {
 	
 	for (int i = 0; i < 2; i++) {
 
-		digitalWrite(D1, HIGH);
-		delay(50);
 		digitalWrite(D1, LOW);
+		delay(50);
+		digitalWrite(D1, HIGH);
 		delay(50);
 
 	}
